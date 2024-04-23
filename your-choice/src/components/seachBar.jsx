710ecddc9c780172d613c26434da1240e24e5782
xiayulin123 <xiayulinsky@gmail.com>
2024-04-23 11:34:09 -0400

    Update the search bar design, add icon and color changed

--- your-choice/src/components/seachBar.jsx
+++ your-choice/src/components/seachBar.jsx
@@ -1,31 +1,46 @@
 import React, { useState } from 'react';
 import './css/searchBar.css';  // Import the CSS for styling
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+import { useTheme } from '@mui/material/styles';
 
 function SearchBar({ onSearch }) {
-  const [query, setQuery] = useState('');  // State to hold the input value
+    const theme = useTheme();
 
-  const handleInputChange = (event) => {
-    setQuery(event.target.value);  // Update the state with the input value
-  };
+    const [isFocused, setIsFocused] = useState(false);
 
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {  
-      onSearch(query);  
-    }
-  };
+    return (
+        <Box sx={{ width: 500,
+            maxWidth: '100%', marginTop: 5 }}>
+        <TextField
+        id="input-with-icon-textfield"
+        label="Search field"
+        type="search"
+        InputProps={{
+            startAdornment: (
+            <InputAdornment position="start" style={{ alignItems: 'center' }}>
+                <SearchIcon sx={{ fontSize: '2rem', color: isFocused ? theme.palette.primary.main : 'inherit' }}/>
+            </InputAdornment>
+            ),
+            style: {
+                height: '3rem',
+                fontSize: '2rem'
+              },
+        }}
+        InputLabelProps={{
+            style: {
+              transform: 'translate(-5px, -15px) scale(1.3)', 
+            },
+          }}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        variant="standard"
+        />
 
-  return (
-    <div className="search-bar">
-      <input
-        type="text"
-        value={query}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-        placeholder="Search..."
-        className="search-input"
-      />
-    </div>
-  );
+        </Box>
+    );
 }
 
 export default SearchBar;
